58c8a983e0870261ae4b86f931161ca45e21ec8b
awjae <djwotmd@gmail.com>
2023-05-06 23:53:04 +0900

    [CLN] : styling css module 제일 안정적인듯 함. 최신버전 next 기준

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,10 @@
 import Image from 'next/image'
 import styles from './page.module.css'
-import AppButton from '@/shared/AppButton'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <AppButton></AppButton>
+      
     </main>
   )
 }
