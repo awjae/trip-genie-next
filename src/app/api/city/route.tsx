cad34bb69bc061b3e353fd631f053a31d971e49d
awjae <djwotmd@gmail.com>
2023-05-09 11:22:29 +0900

    [DOC] : 주석 추가

--- src/app/api/city/route.tsx
+++ src/app/api/city/route.tsx
@@ -1,29 +1,9 @@
 import { NextResponse } from 'next/server';
-import { Pool } from 'pg'
 
-const pool = new Pool({
-  user: 'admin',
-  host: 'svc.sel3.cloudtype.app',
-  database: 'trip_genie',
-  password: 'admin',
-  port: 30641,
-});
+//use client 에 유의미
 
 export async function GET(request: Request) {
-  const client = await pool.connect()
-  try {
-    const result = await client.query('SELECT * FROM cities')
-    if (result.rowCount >= 1) {
-      return NextResponse.json({ message: 'success', result: result.rows })
-    } else {
-      return NextResponse.json({ message: 'fail' })
-    }
-  } catch (err) {
-    console.error(err)
-    return NextResponse.json({ message: '서버 오류가 발생했습니다.' })
-  } finally {
-    client.release()
-  }
+  return NextResponse.json({ message: 'success'})
 }
 
 export async function HEAD(request: Request) {}
--- src/lib/city.ts
+++ src/lib/city.ts
@@ -1,14 +1,18 @@
 import { pool } from "@/utils/pg"
 
+// export async function templete() {
+//   const res = await fetch('/api/city')
+
+//   if (!res.ok) {
+//     throw new Error('Failed to fetch data')
+//   }
+ 
+//   return res.json()
+// }
+
 export async function getCities() {
   const client = await pool.connect()
   const result = await client.query('SELECT * FROM cities')
   return { message: 'success', result: result.rows }
-
-  // if (!res.ok) {
-  //   throw new Error('Failed to fetch data')
-  // }
- 
-  // return res.json()
-  // // return { result : 'true' }
 }
+
