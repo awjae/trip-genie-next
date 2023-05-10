55b521cc592970b001392625677e679ddd6c35c7
awjae <djwotmd@gmail.com>
2023-05-11 00:29:38 +0900

    [ADD] : footer 추가

--- src/components/common/footer.tsx
+++ src/components/common/footer.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import styles from '@/styles/footer.module.css'
 
 function Footer() {
   return (
-    <div>footer</div>
+    <footer className={styles.footer}>
+      <div>CONTACT</div>
+      <div>010-2372-7647</div>
+      <div>djwotmd@gmail.com</div>
+    </footer>
   )
 }
 
