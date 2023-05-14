9b6d07ce656b0f50bad21568a70187d3c8754275
awjae <djwotmd@gmail.com>
2023-05-15 01:34:19 +0900

    [ADD] : 데이터 생성형 SQL 일시 추가

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -19,9 +19,28 @@ function Page() {
 
   const getSpots = async (name: string, id: number) => {
     const response = await fetch(`https://www.myro.co.kr/searchMostSelectedSpots?cityName=${name}`)
-    const result = response.json()
-    console.log(result)
-    // mutate({ name, id, options:  })
+    const result = await response.json()
+    // mutate({ name, id, options: result })
+    let str = "";
+    result.forEach((el: any) => {
+      str += insertText({
+        showingName: el.showingName,
+        googleSearchedName: el.googleSearchedName,
+        address: el.address,
+        lat: el.lat,
+        lng: el.lng,
+        openTime: el.openTime,
+        id: cityId
+      })
+    })
+    console.log(str)
+  }
+
+  const insertText = (obj: any) => {
+    let text = `INSERT INTO public.spots(
+      name, "subName", address, lat, lng, "openTime", "cityId")
+      VALUES ('${obj.showingName.replaceAll('\'','\'\'')}', '${obj.googleSearchedName.replaceAll('\'','\'\'')}', '${obj.address.replaceAll('\'','\'\'')}', ${obj.lat}, ${obj.lng}, '${obj.openTime}', ${obj.id}); `;
+    return text 
   }
 
   useEffect(() => {
@@ -29,7 +48,14 @@ function Page() {
       getSpots(cityData.enName, Number(cityData.id))
     }
   }, [cityData])
+
+  useEffect(() => {
   
+    return () => {
+      
+    }
+  }, [])
+    
 
   return (
     <>
--- src/lib/fetchers.ts
+++ src/lib/fetchers.ts
@@ -5,12 +5,10 @@ export async function getCityForClient(cityId?: number) {
   return result[0];
 }
 export async function postCityForClient(name: string, id: number, data: any) {
-  console.log(data)
-  return true
-  // const res = await fetch(`/api/city`, { 
-  //   method: "POST",
-  //   body: JSON.stringify({ name, id })
-  // });
-  // const { result } = await res.json()
+  const res = await fetch(`/api/city`, { 
+    method: "POST",
+    body: JSON.stringify({ name, id })
+  });
+  const { result } = await res.json()
   return result[0];
 }
