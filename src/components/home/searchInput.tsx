240bc19a2116871f57aaadc3b1ecba7fe3738b20
awjae <djwotmd@gmail.com>
2023-05-17 15:41:09 +0900

    [ADD] : 자동완성기능 추가

--- src/components/home/searchInput.tsx
+++ src/components/home/searchInput.tsx
@@ -2,31 +2,43 @@
 
 import Link from 'next/link'
 import Image from 'next/image'
-import { useState } from 'react'
+import React, { useState } from 'react'
 import styles from '@/app/page.module.css'
 import { CityType } from '@/types/city'
+import { debounce } from '@mui/material'
 
 function SearchInput({ suggestionList }: { suggestionList: CityType[] }) {
   const [inputText, setInputText] = useState('')
   const [isShowSuggestionLayer, setIsShowSuggestionLayer] = useState(false)
+  const [filterdeSuggestionList, setFilterdeSuggestionList] = useState(suggestionList)
 
   const handleSuggestionClick = (city: CityType) => {
     setInputText(city.name)
     setIsShowSuggestionLayer(false)
+    setFilterdeSuggestionList(suggestionList.filter(name => name.name.indexOf(city.name) > -1))
+  }
+
+  const filteredSuggestionFn = (value: string) => {
+    setFilterdeSuggestionList(suggestionList.filter(name => name.name.indexOf(value) > -1))
+  }
+
+  const handleSearchInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    filteredSuggestionFn(e.target.value)
+    setInputText(e.target.value)
   }
 
   return (
     <div>
       <input id="searchInput" type="text" placeholder='여행지를 입력해주세요.' value={inputText} 
-        onChange={(e) => setInputText(e.target.value)}
+        onChange={handleSearchInputChange}
         onFocus={() => setIsShowSuggestionLayer(true)}
       />
       <Link href={'map'}>
         <Image src="/images/icon/search.svg" alt="" width={30} height={30}/>
       </Link>
-      { isShowSuggestionLayer && (
+      { isShowSuggestionLayer && filterdeSuggestionList.length > 0 && (
         <ul className={styles.suggestionContainer}>
-          { suggestionList.map(city => (
+          { filterdeSuggestionList.map(city => (
             <li key={city.id} onClick={() => handleSuggestionClick(city)}>
               { city.name }
             </li>
