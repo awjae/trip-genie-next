b7939cecd1882e934ad6b75ae945364761813604
awjae <djwotmd@gmail.com>
2023-06-05 21:32:04 +0900

    [ADD] : 우측 상단 검색 컴포넌트 css 작업

--- src/components/map/mapSearch.tsx
+++ src/components/map/mapSearch.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import styles from '@/styles/map.module.css'
+import Image from 'next/image'
 
 function MapSearch() {
   return (
     <div className={styles.mapSearch}>
       <input type="text" placeholder='검색어'/>
+      <Image src='/images/icon/search.png' alt="" width={24} height={24}/>
     </div>
   )
 }
