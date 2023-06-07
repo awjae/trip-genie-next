d9c99db82fb2b16be56e73e04d3fa9c90b597bc4
awjae <djwotmd@gmail.com>
2023-06-08 00:47:12 +0900

    [ADD] : search Input ArrowDown 키 이벤트 추가

--- src/components/home/searchInput.tsx
+++ src/components/home/searchInput.tsx
@@ -7,23 +7,24 @@ import styles from '@/app/page.module.css'
 import { CityType } from '@/types/city'
 import { useRouter } from 'next/navigation'
 import { useDebounceFunction } from '@/utils/utils'
+import classNames from 'classnames'
 
 function SearchInput({ suggestionList }: { suggestionList: CityType[] }) {
   const [inputText, setInputText] = useState('')
   const [isShowSuggestionLayer, setIsShowSuggestionLayer] = useState(false)
-  const [filterdeSuggestionList, setFilterdeSuggestionList] = useState(suggestionList)
+  const [filterdeSuggestionList, setFilterdeSuggestionList] = useState(suggestionList.map(suggestion => ({ ...suggestion, select: false })))
   const [selectedCity, setSelectedCity] = useState<CityType|null>(null)
   const router = useRouter()
 
   const handleSuggestionClick = (city: CityType) => {
     setInputText(city.name)
     setIsShowSuggestionLayer(false)
-    setFilterdeSuggestionList(suggestionList.filter(name => name.name.indexOf(city.name) > -1))
+    setFilterdeSuggestionList(suggestionList.filter(name => name.name.indexOf(city.name) > -1).map(suggestion => ({ ...suggestion, select: false })))
     setSelectedCity(city)
   }
 
   const filteredSuggestionFn = (value: string) => {
-    setFilterdeSuggestionList(suggestionList.filter(name => name.name.indexOf(value) > -1))
+    setFilterdeSuggestionList(suggestionList.filter(name => name.name.indexOf(value) > -1).map(suggestion => ({ ...suggestion, select: false })))
   }
 
   const debouncedFetchSuggestions = useDebounceFunction(filteredSuggestionFn, 300)
@@ -46,7 +47,34 @@ function SearchInput({ suggestionList }: { suggestionList: CityType[] }) {
     if (e.key === 'Enter' && filterdeSuggestionList.length === 1) {
       handleSuggestionClick(filterdeSuggestionList[0])
       goMap()
+      return
     }
+    if (e.key === 'ArrowDown') {
+      const findSelected = filterdeSuggestionList.findIndex(suggestion => suggestion.select)
+      if (findSelected < 0) {
+        setFilterdeSuggestionList(filterdeSuggestionList.map((suggestion, idx) => {
+          if (idx === 0) {
+            return {
+              ...suggestion,
+              select: true
+            }
+          }
+          return { ...suggestion }
+        }))
+        return
+      }
+
+      setFilterdeSuggestionList(filterdeSuggestionList.map((suggestion, idx) => {
+        if (idx === findSelected + 1) {
+          return {
+            ...suggestion,
+            select: true
+          }
+        }
+        return { ...suggestion, select: false }
+      }))
+    }
+    
   }
 
   return (
@@ -62,7 +90,7 @@ function SearchInput({ suggestionList }: { suggestionList: CityType[] }) {
       { isShowSuggestionLayer && filterdeSuggestionList.length > 0 && (
         <ul className={styles.suggestionContainer}>
           { filterdeSuggestionList.map(city => (
-            <li key={city.id} onClick={() => handleSuggestionClick(city)}>
+            <li key={city.id} onClick={() => handleSuggestionClick(city)} className={classNames({ [styles.select]: city.select })}>
               { city.name }
             </li>
           ))}
